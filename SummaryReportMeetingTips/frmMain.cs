5a6cf1ca3ba2b2f3869f24942540295313621213
Edward <edward_song@yeah.net>
2017-05-09 09:05:50 +0800

    fix weeklyworkfreq decimal...

--- SummaryReportMeetingTips/frmMain.cs
+++ SummaryReportMeetingTips/frmMain.cs
@@ -312,7 +312,9 @@ namespace SummaryReportMeetingTips
                         return;
                 }
 
+                this.Enabled = false;
                 updateRawData(ds, comboSheetList, p.WorkType.Meeting);
+                this.Enabled = true;
             }
 
 
@@ -458,7 +460,7 @@ reviewer) VALUES (@_depcode,
                             cmd.Parameters.Add(new SQLiteParameter(@"_ismywork", DbType.String));
                             cmd.Parameters.Add(new SQLiteParameter(@"_vanva", DbType.String));
                             cmd.Parameters.Add(new SQLiteParameter(@"_singleworktime", DbType.Decimal));
-                            cmd.Parameters.Add(new SQLiteParameter(@"_weeklyworkfreq", DbType.Int16));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_weeklyworkfreq", DbType.Decimal));
                             cmd.Parameters.Add(new SQLiteParameter(@"_weeklyworktime", DbType.Decimal));
                             cmd.Parameters.Add(new SQLiteParameter(@"_monthlyworktime", DbType.Decimal));
                             cmd.Parameters.Add(new SQLiteParameter(@"_caller", DbType.String));
@@ -482,7 +484,116 @@ reviewer) VALUES (@_depcode,
                             cmd.Parameters[9].Value = ds.Tables[combo.SelectedIndex].Rows[i]["本职/非本职"].ToString();
                             cmd.Parameters[10].Value = ds.Tables[combo.SelectedIndex].Rows[i]["VA/NVA  (部级评核)"].ToString();
                             cmd.Parameters[11].Value = decimal.Round(Convert.ToDecimal(ds.Tables[combo.SelectedIndex].Rows[i]["单次工作时间（分钟）"]) / 60, 4);
-                            cmd.Parameters[12].Value = Convert.ToInt16(ds.Tables[combo.SelectedIndex].Rows[i]["周工作频率（次）"]);
+                            cmd.Parameters[12].Value = Convert.ToDecimal(ds.Tables[combo.SelectedIndex].Rows[i]["周工作频率（次）"]);
+                            cmd.Parameters[13].Value = decimal.Round(Convert.ToDecimal(ds.Tables[combo.SelectedIndex].Rows[i]["周工時(分)"]) / 60, 4);
+                            cmd.Parameters[14].Value = decimal.Round(Convert.ToDecimal(ds.Tables[combo.SelectedIndex].Rows[i]["月工时（分）"]) / 60, 4);
+                            cmd.Parameters[15].Value = ds.Tables[combo.SelectedIndex].Rows[i]["召集者"].ToString();
+                            cmd.Parameters[16].Value = ds.Tables[combo.SelectedIndex].Rows[i]["召集者部門"].ToString();
+                            cmd.Parameters[17].Value = ds.Tables[combo.SelectedIndex].Rows[i]["會議要求者級別"].ToString();
+                            cmd.Parameters[18].Value = ds.Tables[combo.SelectedIndex].Rows[i]["改善方法"].ToString();
+                            cmd.Parameters[19].Value = ds.Tables[combo.SelectedIndex].Rows[i]["節省周工時"].ToString();
+                            cmd.Parameters[20].Value = ds.Tables[combo.SelectedIndex].Rows[i]["描述"].ToString();
+                            //
+                            string _date = ds.Tables[combo.SelectedIndex].Rows[i]["review date"].ToString();
+                            if (string.IsNullOrEmpty(_date))
+                            {
+                                cmd.Parameters[21].Value = "";
+                            }
+                            else
+                            {
+                                cmd.Parameters[21].Value = Convert.ToDateTime(_date).ToString("yyyy-MM-dd");
+                            }
+
+                            cmd.Parameters[22].Value = ds.Tables[combo.SelectedIndex].Rows[i]["reviewer"].ToString();
+
+                        }
+
+                        if (worktype == p.WorkType.Report)
+                        {
+                            cmd.CommandText =
+                                @"INSERT INTO t_meetingrawdata (depcode,
+seccode,
+opid,
+engname,
+meetingtype,
+workcontent,
+workdetail,
+worktype,
+isinworkbook,
+ismywork,
+vanva,
+singleworktime,
+weeklyworkfreq,
+weeklyworktime,
+monthlyworktime,
+caller,
+callerdep,
+callerlevel,
+optimizemethod,
+weeklysavetime,
+description,
+reviewdate,
+reviewer) VALUES (@_depcode,
+@_seccode,
+@_opid,
+@_engname,
+@_meetingtype,
+@_workcontent,
+@_workdetail,
+@_worktype,
+@_isinworkbook,
+@_ismywork,
+@_vanva,
+@_singleworktime,
+@_weeklyworkfreq,
+@_weeklyworktime,
+@_monthlyworktime,
+@_caller,
+@_callerdep,
+@_callerlevel,
+@_optimizemethod,
+@_weeklysavetime,
+@_description,
+@_reviewdate,
+@_reviewer)";
+
+                            cmd.Parameters.Add(new SQLiteParameter("@_depcode", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_seccode", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_opid", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_engname", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_meetingtype", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_workcontent", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_workdetail", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_worktype", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_isinworkbook", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_ismywork", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_vanva", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_singleworktime", DbType.Decimal));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_weeklyworkfreq", DbType.Decimal));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_weeklyworktime", DbType.Decimal));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_monthlyworktime", DbType.Decimal));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_caller", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_callerdep", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_callerlevel", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_optimizemethod", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_weeklysavetime", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_description", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_reviewdate", DbType.String));
+                            cmd.Parameters.Add(new SQLiteParameter(@"_reviewer", DbType.String));
+                            //
+                            cmd.Parameters[0].Value = ds.Tables[combo.SelectedIndex].Rows[i]["部門代碼"].ToString();
+                            cmd.Parameters[1].Value = ds.Tables[combo.SelectedIndex].Rows[i]["課別代碼"].ToString();
+                            cmd.Parameters[2].Value = ds.Tables[combo.SelectedIndex].Rows[i]["工號"].ToString();
+                            cmd.Parameters[3].Value = ds.Tables[combo.SelectedIndex].Rows[i]["英文姓名"].ToString();
+                            cmd.Parameters[4].Value = ds.Tables[combo.SelectedIndex].Rows[i]["会议类型"].ToString();
+                            cmd.Parameters[5].Value = ds.Tables[combo.SelectedIndex].Rows[i]["工作內容"].ToString();
+                            cmd.Parameters[6].Value = ds.Tables[combo.SelectedIndex].Rows[i]["工作細目"].ToString();
+                            cmd.Parameters[7].Value = ds.Tables[combo.SelectedIndex].Rows[i]["工作分类"].ToString();
+                            cmd.Parameters[8].Value = ds.Tables[combo.SelectedIndex].Rows[i]["是否在岗位说明书"].ToString();
+                            cmd.Parameters[9].Value = ds.Tables[combo.SelectedIndex].Rows[i]["本职/非本职"].ToString();
+                            cmd.Parameters[10].Value = ds.Tables[combo.SelectedIndex].Rows[i]["VA/NVA  (部级评核)"].ToString();
+                            cmd.Parameters[11].Value = decimal.Round(Convert.ToDecimal(ds.Tables[combo.SelectedIndex].Rows[i]["单次工作时间（分钟）"]) / 60, 4);
+                            cmd.Parameters[12].Value = Convert.ToDecimal(ds.Tables[combo.SelectedIndex].Rows[i]["周工作频率（次）"]);
                             cmd.Parameters[13].Value = decimal.Round(Convert.ToDecimal(ds.Tables[combo.SelectedIndex].Rows[i]["周工時(分)"]) / 60, 4);
                             cmd.Parameters[14].Value = decimal.Round(Convert.ToDecimal(ds.Tables[combo.SelectedIndex].Rows[i]["月工时（分）"]) / 60, 4);
                             cmd.Parameters[15].Value = ds.Tables[combo.SelectedIndex].Rows[i]["召集者"].ToString();
@@ -503,13 +614,11 @@ reviewer) VALUES (@_depcode,
                             }
 
 
-                           
+
                             cmd.Parameters[22].Value = ds.Tables[combo.SelectedIndex].Rows[i]["reviewer"].ToString();
 
-                        }
 
-                        if (worktype == p.WorkType.Report)
-                        {
+
                         }
 
 
@@ -527,7 +636,7 @@ reviewer) VALUES (@_depcode,
                 }
                 conn.Close ();
                 sw.Stop();
-                MessageBox.Show(string.Format("Insert into database {0} records,used time:{1}", ds.Tables[combo.SelectedIndex].Rows.Count, sw.Elapsed.ToString()));
+                MessageBox.Show(string.Format("Insert into database {0} records,used time(ms):{1}", ds.Tables[combo.SelectedIndex].Rows.Count, sw.ElapsedMilliseconds.ToString ()));
 
             }
 
